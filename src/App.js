22285a60d2a14e4fa9fc38d190d047078ec4839a
Saul Calleja <scalleja12345@gmail.com>
2022-02-07 16:22:26 -0500

    Show Info from api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import { CLIENT_URL } from "./services/rando_user";
+import axios from "axios";
 
 function App() {
+  const [userDataName, setUserDataName] = useState([]);
+
+  const [userDataEmail, setUserDataEmail] = useState([]);
+
+  useEffect(() => {
+    fetchUserData();
+  }, []);
+  const fetchUserData = async () => {
+    try {
+      const response = await axios.get(CLIENT_URL);
+      const title = response.data.results[0].name.title;
+      const name = response.data.results[0].name.first;
+      const last = response.data.results[0].name.last;
+      console.log(name);
+      setUserDataName(title + " " + name + " " + last);
+      const email = response.data.results[0].email;
+      console.log(email);
+      setUserDataEmail(email);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={fetchUserData}>Click to Refresh</button>
+      <h1>{userDataName}</h1>
+      <h2>{userDataEmail}</h2>
     </div>
   );
 }
